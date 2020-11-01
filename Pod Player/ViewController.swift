781c02e81f370d4fd085d29656320537fd8ee127
Virgil DINU <vir_dinu@yahoo.com>
2020-11-01 06:19:14 +0200

    Initial Commit

---
+++ Pod Player/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pod Player
+//
+//  Created by Virgil DINU on 01/11/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
