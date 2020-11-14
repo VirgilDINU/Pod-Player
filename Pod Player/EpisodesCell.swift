97af690a9c7b926f6b2561e37abafe8c4bab6c6b
Virgil DINU <vir_dinu@yahoo.com>
2020-11-14 19:59:40 +0200

    FINAL!!!

---
+++ Pod Player/EpisodesCell.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodesCell.swift
+//  Pod Player
+//
+//  Created by Virgil DINU on 14.11.2020.
+//
+
+import Cocoa
+
+class EpisodesCell: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
